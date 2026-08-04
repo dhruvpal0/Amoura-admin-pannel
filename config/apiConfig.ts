848e653175c31d9a0666038e62c6c19b362b59f7
dhruvpal0 <pal664908@gmail.com>
2026-08-04 16:54:59 +0530

    first commit

--- config/apiConfig.ts
+++ config/apiConfig.ts
@@ -1,5 +1,7 @@
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/v1';
 
+export const API_BASE_URL = 'https://amoura-backend-xlv0.onrender.com/v1';
+
 export default {
   API_BASE_URL,
 };
